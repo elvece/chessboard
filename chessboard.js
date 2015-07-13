41ee8f02fde502c69aa95ff2744b12ccff3f2b1c
elvece <lvcifferello@gmail.com>
2015-07-13 11:41:34 -0600

    added solutions from class

--- chessboard.js
+++ chessboard.js
@@ -1,13 +1,32 @@
-var line = " "
-
+//mike's solution//
 for (var i=0; i<8; i++) {
-  if (i%2===0){
-    //something to happen
+
+  var line = "";
+
+for (var j=0; j<8; j++) {
+
+  var total = i + j;
+
+  if (total % 2 === 0){
+    line += " ";
+  } else {
+    line += "#";
   }
-  else {
-    //something different to happen
-  }
-  line += "#";
 }
 
-console.log (line)
+console.log (line);
+}
+
+//alt solution//
+var odd=" # # # #\n";
+var even= "# # # #\n";
+var result= "";
+for (var i=0; i<8; i++) {
+
+  if (i%2===0) {
+    result += odd;
+  } else {
+    result += even;
+  }
+}
+ console.log (result)
