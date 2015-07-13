ed699a98e26a41667132fd9af28e6ab04b40d493
elvece <lvcifferello@gmail.com>
2015-07-13 09:23:21 -0600

    first commit, added files, outlined problem

---
+++ chessboard.js
@@ -0,0 +1,13 @@
+var line = " "
+
+for (var i=0; i<8; i++) {
+  if (i%2===0){
+    //something to happen
+  }
+  else {
+    //something different to happen
+  }
+  line += "#";
+}
+
+console.log (line)
